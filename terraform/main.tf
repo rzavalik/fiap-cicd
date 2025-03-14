285846f1a6e4f9ef2e9cc498e7f11db1829e2219
Rodrigo Castro <zavalik@live.com>
2025-03-14 16:54:50 -0300

    Alterando o Terraform para subir corretamente a instancia

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,16 +10,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_s3_object" "ssh_key" {
-  bucket = "zavalik-terraformstate"
-  key    = "ssh/id_rsa.pub"
-}
-
-resource "aws_key_pair" "ssh_key" {
-  key_name   = "ssh-key"
-  public_key = data.aws_s3_object.ssh_key.bucket
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -31,14 +21,29 @@ resource "aws_subnet" "main" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
-  tags = {
-    Name = "main-subnet"
-  }
 }
 
-# Security Group to allow SSH and HTTP access
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
 resource "aws_security_group" "allow_all" {
-  name_prefix = "allow_all"
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 80
@@ -46,39 +51,104 @@ resource "aws_security_group" "allow_all" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# Create the EC2 instance with SSH key and VPC
+resource "aws_iam_role" "ec2_ecr_role" {
+  name = "EC2ECRAccessRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecr_policy" {
+  name   = "ECRAccessPolicy"
+  role   = aws_iam_role.ec2_ecr_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_ecr_profile" {
+  name = "EC2ECRAccessProfile"
+  role = aws_iam_role.ec2_ecr_role.name
+}
+
 resource "aws_instance" "helloworld_app" {
-  ami           = var.ec2_ami
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-  security_groups = [aws_security_group.allow_all.name]
-  
-  key_name = aws_key_pair.ssh_key.key_name
-  
-  # Associate the instance with the created subnet
+  ami                    = var.ec2_ami
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  security_groups        = [aws_security_group.allow_all.id]
   associate_public_ip_address = true
-  
+  iam_instance_profile   = aws_iam_instance_profile.ec2_ecr_profile.name
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "optional"
+  }
+
   user_data = <<-EOF
               #!/bin/bash
-              # Garante o Docker instalado
               sudo apt update -y
-              sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+              # Instala docker e pre-requisitos
+              sudo apt install -y apt-transport-https ca-certificates curl software-properties-common unzip
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
               sudo apt update -y
               sudo apt install -y docker-ce
               sudo systemctl start docker
               sudo systemctl enable docker
               sudo usermod -aG docker $USER
+
+              # Instala o AWS CLI
+              sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              sudo unzip -q awscliv2.zip
+              sudo ./aws/install
+
               # Executa a app
-              docker pull ${var.ecr_uri}:latest
-              docker run -d -p 80:80 --name helloworld_app ${var.ecr_uri}:latest
+              sudo aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${var.ecr_uri}
+              sudo docker pull ${var.ecr_uri}:latest
+              sudo docker run -d -p 80:8080 --name helloworld_app ${var.ecr_uri}:latest
               EOF
+
+  depends_on = [aws_security_group.allow_all]
 }
 
-# Output the public IP for SSH access
 output "instance_public_ip" {
   value = aws_instance.helloworld_app.public_ip
 }
